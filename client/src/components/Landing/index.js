783e4467912d4f39ac7491ee8c086dd686dd8e85
Haley Schott <hschott@uwaterloo.ca>
2023-03-16 16:27:03 -0400

    added static informaiton

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -95,7 +95,44 @@ export default function Landing() {
             Learn More
           </Button>
         </div>
+        
+        <p style={{marginTop: '75px'}}></p>
+        <div align="center">
+          <Card
+            style={{
+              color: 'white',
+              backgroundColor: '#001833',
+              height: '90px',
+              width: '900px',
+              opacity: '75%',
+              marginBottom: '50px',
+            }}
+          >
+            <Typography
+              align="center"
+              variant="h7"
+              component="div"
+              gutterBottom
+              style={{
+                fontFamily: 'Roboto',
+                color: 'white',
+                opacity: '100%',
+                align: 'center',
+              }}
+            >
+              We know that Waterloo students undergo a tedious preparation process before classes 
+              start for the term. Everything from finding a place, paying thousands in tuition to having 
+              to figure out what they want for lunch. Planning a budget that includes residence, food, entertainment, 
+              and even travel can take time. Currently, many students have to calculate their term budget by hand, 
+              often leading to a suboptimal solution. This is where our proposed tool, “The Hungry Warrior,” comes in. 
+              We created this program with the goal of helping University of Waterloo students just like you. 
+            </Typography>
+          </Card>
+        </div>
+          
       </div>
     </div>
-  );
+
+
+  )
 }
