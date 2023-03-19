73f6b992551ba64451b9a3a2e487618bf72a29a6
npiplias <npiplias@uwaterloo.ca>
2023-03-19 19:28:41 -0400

    Added info to restaurant info page

--- client/src/components/InfoRestaurant/index.js
+++ client/src/components/InfoRestaurant/index.js
@@ -64,6 +64,10 @@ const InfoRestaurant = () => {
                     <Typography align='center' variant="h10" color="inherit" style={{ marginLeft: '30px' }}>
                     For over 60 years we've been proud to serve our fellow Canadians only the best ingredients and now more than ever our commitment to 100% Canadian Quality remains the same.
                     You could say, we're fans of what grows in our own backyard. So Canada, are you ready to help us on our mission to keep this country a beautiful thing?
+                    <p>Address: 170 University Ave W Unit 10, Waterloo</p>
+                            <p>Cost: $7</p>
+                            <p>Distance from E7: 160M</p>
+                            <p>Rating: 3.9/5</p>
                     </Typography>
                 </div>
 
@@ -126,6 +130,10 @@ const InfoRestaurant = () => {
                         <Typography align='center' variant="h10" color="inherit" style={{ marginLeft: '30px' }}>
                         Lobster Burger Bar is our name. Fresh Atlantic Lobster and Angus Beef is our Game!
                         It’s hard to imagine two of the world’s most delicious foods now available in Waterloo, within walking distance to both universities and high-tech companies.
+                        <p>Address: 170 University Ave W, Waterloo</p>
+                            <p>Cost: $25</p>
+                            <p>Distance from E7: 160M</p>
+                            <p>Rating: 4.4/5</p>
                         </Typography>
                     </div>
 
@@ -168,7 +176,7 @@ const InfoRestaurant = () => {
     </div></>
 
     <div align='center'>
-        <Card style={{ color: 'black', backgroundColor: 'white', height: '50vh', width: '200vh', marginTop: '50px', marginBottom: '20px', boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)" }}>
+        <Card style={{ color: 'black', backgroundColor: 'white', height: '56vh', width: '200vh', marginTop: '50px', marginBottom: '20px', boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)" }}>
             <Grid container spacing={2} columns={12}>
                 <Grid item xs={5}>
                     <div align='left'>
@@ -188,6 +196,10 @@ const InfoRestaurant = () => {
                         <Typography align='center' variant="h10" color="inherit" style={{ marginLeft: '30px' }}>
                         Williams Fresh Café is your all-day, every day café for delicious food and signature drinks made-to-order from real, fresh ingredients and served to your table with a real smile. Our cafés offer a stylish yet casual setting, each with its own local flavour.
                         Whether conducting a morning meeting over breakfast, catching up with a friend over lunch, indulging in a mid-afternoon coffee break with colleagues, or sharing an evening drink and dessert with a loved one, Williams Fresh Café will always make a fresh difference to your day.
+                        <p>Address: 170 University Ave W, Waterloo</p>
+                            <p>Cost: $8</p>
+                            <p>Distance from E7: 160M</p>
+                            <p>Rating: 3.5/5</p>
                         </Typography>
                     </div>
 
@@ -249,6 +261,10 @@ const InfoRestaurant = () => {
                     <div style={{ marginTop: '20px', marginRight: '30px', textAlign: 'Left' }}>
                         <Typography align='center' variant="h10" color="inherit" style={{ marginLeft: '30px' }}>
                         From our customizable and signature sandwiches to wraps and salads, Subway is simply an excellent choice when it comes to freshly made, convenient and affordable offerings. We’re one of the world’s largest restaurant brands for a reason, serving up a craveable menu with quality options to our millions of fans every day.
+                        <p>Address: 200 University Ave W, Waterloo</p>
+                            <p>Cost: $8</p>
+                            <p>Distance from E7: 750M</p>
+                            <p>Rating: 4.0/5</p>
                         </Typography>
                     </div>
 
@@ -291,7 +307,7 @@ const InfoRestaurant = () => {
     </div></>
 
     <div align='center'>
-        <Card style={{ color: 'black', backgroundColor: 'white', height: '50vh', width: '200vh', marginTop: '50px', marginBottom: '20px', boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)" }}>
+        <Card style={{ color: 'black', backgroundColor: 'white', height: '59vh', width: '200vh', marginTop: '50px', marginBottom: '20px', boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)" }}>
             <Grid container spacing={2} columns={12}>
                 <Grid item xs={5}>
                     <div align='left'>
@@ -311,6 +327,10 @@ const InfoRestaurant = () => {
                         <Typography align='center' variant="h10" color="inherit" style={{ marginLeft: '30px' }}>
                         Welcome to The Pub on King in Uptown Waterloo. Home of the $7.95 food menu! With Opening our doors more than five years ago, we have established ourselves as the-place-to-be in Uptown Waterloo. During the day you can stop by for a quick and quiet lunch of a variety of pub fare with inspiration from many British Classics!
                         A great place for your happy-hour hang out after work with a variety of appetizers and daily drink features. Our nightlife atmosphere turns more upbeat with daily live entertainment appealing to all ages. The affordable and social hotspot that people can’t stop talking about!
+                        <p>Address: 77 King Street N, Waterloo</p>
+                            <p>Cost: $8</p>
+                            <p>Distance from E7: 1900M</p>
+                            <p>Rating: 4.0/5</p>
                         </Typography>
                     </div>
 
@@ -372,6 +392,10 @@ const InfoRestaurant = () => {
                     <div style={{ marginTop: '20px', marginRight: '30px', textAlign: 'Left' }}>
                         <Typography align='center' variant="h10" color="inherit" style={{ marginLeft: '30px' }}>
                         Inspired by a passion for serving traditional home style dishes in a friendly, comfortable, and modern atmosphere, Korner Kitchen Breakfast & Lunch Eatery is the proud result of over 30 years in the local dining industry.
+                        <p>Address: 183 Weber Street N, Waterloo</p>
+                            <p>Cost: $11</p>
+                            <p>Distance from E7: 2200M</p>
+                            <p>Rating: 4.6/5</p>
                         </Typography>
                     </div>
 
@@ -414,7 +438,7 @@ const InfoRestaurant = () => {
     </div></>
 
     <div align='center'>
-        <Card style={{ color: 'black', backgroundColor: 'white', height: '50vh', width: '200vh', marginTop: '50px', marginBottom: '20px', boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)" }}>
+        <Card style={{ color: 'black', backgroundColor: 'white', height: '54vh', width: '200vh', marginTop: '50px', marginBottom: '20px', boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)" }}>
             <Grid container spacing={2} columns={12}>
                 <Grid item xs={5}>
                     <div align='left'>
@@ -434,6 +458,10 @@ const InfoRestaurant = () => {
                         <Typography align='center' variant="h10" color="inherit" style={{ marginLeft: '30px' }}>
                         If you’re craving tasty Canadian and Mediterranean meals in Waterloo, then Johnny Fresco is the place to be. Come check out our energetic and lively atmosphere for a truly authentic dining experience in beautiful Waterloo.
                         Our food is made with only the freshest ingredients available and our portions are so big you will never walk away hungry.
+                        <p>Address: 244 Weber Street N, Waterloo</p>
+                            <p>Cost: $9</p>
+                            <p>Distance from E7: 2000M</p>
+                            <p>Rating: 4.5/5</p>
                         </Typography>
                     </div>
 
@@ -476,7 +504,7 @@ const InfoRestaurant = () => {
     </div></>
 
     <div align='center'>
-        <Card style={{ color: 'black', backgroundColor: 'white', height: '50vh', width: '200vh', marginTop: '50px', marginBottom: '20px', boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)" }}>
+        <Card style={{ color: 'black', backgroundColor: 'white', height: '53vh', width: '200vh', marginTop: '50px', marginBottom: '20px', boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)" }}>
             <Grid container spacing={2} columns={12}>
                 <Grid item xs={5}>
                     <div align='left'>
@@ -496,6 +524,10 @@ const InfoRestaurant = () => {
                         <Typography align='center' variant="h10" color="inherit" style={{ marginLeft: '30px' }}>
                         Our USP has always been the same: an authentic shawarma experience with the finest ingredients available. This mission has led us to achieve 58+ awards and #1 rankings from Google, TripAdvisor, The Record and other respected bodies.
                         This has all been due to the love and support of our amazing customers, whose commitment to our brand proves that taste and authenticity matters above everything else. 
+                        <p>Address: 160 University Ave W, Waterloo</p>
+                            <p>Cost: $11</p>
+                            <p>Distance from E7: 400M</p>
+                            <p>Rating: 4.7/5</p>
                         </Typography>
                     </div>
 
@@ -538,7 +570,7 @@ const InfoRestaurant = () => {
     </div></>
 
     <div align='center'>
-        <Card style={{ color: 'black', backgroundColor: 'white', height: '50vh', width: '200vh', marginTop: '50px', marginBottom: '20px', boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)" }}>
+        <Card style={{ color: 'black', backgroundColor: 'white', height: '56vh', width: '200vh', marginTop: '50px', marginBottom: '20px', boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)" }}>
             <Grid container spacing={2} columns={12}>
                 <Grid item xs={5}>
                     <div align='left'>
@@ -558,6 +590,10 @@ const InfoRestaurant = () => {
                         <Typography align='center' variant="h10" color="inherit" style={{ marginLeft: '30px' }}>
                         The Lazeez story begins with the universal passion for great taste. Our founders, while each having different and unique stories, backgrounds and personalities, found a strong connection over their common love for great tasting Middle Eastern food. 
                         Spiced to perfection, marinated right, and topped with our signature sauces, our food hits the local taste buds hard. Lazeez has since become the go-to place for those who love the delicious flavours of the Middle East; and as we have learned, that’s pretty much everyone.
+                        <p>Address: 170 University Ave W, Waterloo</p>
+                            <p>Cost: $13</p>
+                            <p>Distance from E7: 230M</p>
+                            <p>Rating: 4.4/5</p>
                         </Typography>
                     </div>
 
@@ -600,7 +636,7 @@ const InfoRestaurant = () => {
     </div></>
 
     <div align='center'>
-        <Card style={{ color: 'black', backgroundColor: 'white', height: '50vh', width: '200vh', marginTop: '50px', marginBottom: '20px', boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)" }}>
+        <Card style={{ color: 'black', backgroundColor: 'white', height: '54vh', width: '200vh', marginTop: '50px', marginBottom: '20px', boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)" }}>
             <Grid container spacing={2} columns={12}>
                 <Grid item xs={5}>
                     <div align='left'>
@@ -620,6 +656,10 @@ const InfoRestaurant = () => {
                         <Typography align='center' variant="h10" color="inherit" style={{ marginLeft: '30px' }}>
                         In 1964, the first Tim Hortons restaurant in Hamilton, Ontario opened its doors and Canadians have been ordering Tim Hortons iconic Original Blend coffee, Double-Double coffees, Donuts and Timbits ever since. 
                         Over the years, Tim Hortons has captured the hearts and taste buds of Canadians. More than a coffee and bake shop, Tim Hortons is part of the fabric of Canada and a proud symbol of our country and its values.
+                        <p>Address: SLC Great Hall, 200 University Ave W, Waterloo</p>
+                            <p>Cost: $5</p>
+                            <p>Distance from E7: 750M</p>
+                            <p>Rating: 2.8/5</p>
                         </Typography>
                     </div>
 
@@ -681,6 +721,10 @@ const InfoRestaurant = () => {
                     <div style={{ marginTop: '20px', marginRight: '30px', textAlign: 'Left' }}>
                         <Typography align='center' variant="h10" color="inherit" style={{ marginLeft: '30px' }}>
                         Cozy little diner serving all day breakfast and home made burgers.
+                        <p>Address: 140 University Ave W, Waterloo</p>
+                            <p>Cost: $7</p>
+                            <p>Distance from E7: 550M</p>
+                            <p>Rating: 4.3/5</p>
                         </Typography>
                     </div>
 
@@ -724,7 +768,7 @@ const InfoRestaurant = () => {
     
     
     <div align='center'>
-        <Card style={{ color: 'black', backgroundColor: 'white', height: '50vh', width: '200vh', marginTop: '50px', marginBottom: '20px', boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)" }}>
+        <Card style={{ color: 'black', backgroundColor: 'white', height: '54vh', width: '200vh', marginTop: '50px', marginBottom: '20px', boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)" }}>
             <Grid container spacing={2} columns={12}>
                 <Grid item xs={5}>
                     <div align='left'>
@@ -744,6 +788,10 @@ const InfoRestaurant = () => {
                         <Typography align='center' variant="h10" color="inherit" style={{ marginLeft: '30px' }}>
                         At the Pita Pit, we’re proud of what we put into our pitas, and equally proud of what we don’t. Nothing’s deep-fried, battered or grilled in fat. 
                         We don’t add salt unless you tell us you want it. And we don’t stuff our good-for-you, great-tasting fillings in big wads of bread as thick as a mattress. Welcome to fresh thinking and healthy eating at the Pita Pit, a proudly Canadian, no b.s. place to eat.
+                        <p>Address: SLC Great Hall, 200 University Ave W, Waterloo</p>
+                            <p>Cost: $7</p>
+                            <p>Distance from E7: 800M</p>
+                            <p>Rating: 2.9/5</p>
                         </Typography>
                     </div>
 
