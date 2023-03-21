5037058889b9216b778acef398bc209f506e045c
aryanxved <aved@uwaterloo.ca>
2023-03-21 19:03:17 -0400

    Entertainment Filters added, UI updates

--- client/src/components/Entertainment/index.js
+++ client/src/components/Entertainment/index.js
@@ -7,12 +7,26 @@ import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import {makeStyles} from '@material-ui/core/styles';
+import {
+  Card,
+  CardActions,
+  CardContent,
+  TextField,
+  Radio,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  FormHelperText,
+  CssBaseline,
+} from '@material-ui/core/';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import { FormControl, MenuItem } from '@material-ui/core';
+import './index.css';
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -25,17 +39,65 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+function Title() {
+
+  return (
+    <div>
+      <div>
+        <CssBaseline />
+        <div>
+          <Card style={{color: 'white', backgroundColor: '#001833'}}>
+            <Typography
+              align="center"
+              variant="h3"
+              component="div"
+              gutterBottom
+              style={{fontFamily: 'Roboto'}}
+            >
+              Entertainment
+            </Typography>
+          </Card>
+        </div>
+        </div>
+        </div>
+        )
+    }
+
+    function PrefTitle() {
+
+      return (
+        <div>
+          <div>
+            <CssBaseline />
+            <div>
+                <Typography
+                  align="left"
+                  variant="h5"
+                  component="div"
+                  gutterBottom
+                  style={{fontFamily: 'Roboto', color:"white", marginLeft: '20px'}}
+                >
+                  Let's get your Entertainment preferences:
+                </Typography>
+            </div>
+            </div>
+            </div>
+            )
+        }
+
 const Entertainment = () => {
   const classes = useStyles();
 
   const SelectBudget = () => {
     return(
-      <FormControl fullWidth>
-        <InputLabel id="select-distance">Select your Budget</InputLabel>
+      <FormControl style={{backgroundColor: 'white', width: '300px', borderRadius: '15px', textAlign: "center"}}>
+        <InputLabel id="select-distance" style={{color: '#001833', marginLeft: '10px'}}>Select your Budget</InputLabel>
           <Select
-            lableID="select-budget"
+            labelID="select-budget"
             id="select-budget"
-            lable="Select your Budget"
+            label="Select your Budget"
+            variant="outlined"
+            align="center"
           >
             <MenuItem value={10}>$0</MenuItem>
             <MenuItem value={20}>$10</MenuItem>
@@ -52,12 +114,13 @@ const Entertainment = () => {
 
   const SelectActivityLevel = () => {
     return(
-      <FormControl fullWidth>
-        <InputLabel id="select-activity-level">Select Activity Level</InputLabel>
+      <FormControl style={{backgroundColor: 'white', width: '300px', borderRadius: '15px', marginLeft: '20px'}}>
+        <InputLabel id="select-activity-level" style={{color: '#001833', marginLeft: '10px'}}>Select Activity Level</InputLabel>
           <Select
-            lableID="select-activity-level"
+            labelID="select-activity-level"
             id="select-activity-level"
-            lable="Select Activity Level"
+            label="Select Activity Level"
+            variant="outlined"
           >
             <MenuItem value={10}>Low</MenuItem>
             <MenuItem value={20}>Medium</MenuItem>
@@ -69,12 +132,13 @@ const Entertainment = () => {
 
   const SelectType = () => {
     return(
-      <FormControl fullWidth>
-        <InputLabel id="select-type">Select Activity Type</InputLabel>
+      <FormControl style={{backgroundColor: 'white', width: '300px', borderRadius: '15px', marginLeft: '20px'}}>
+        <InputLabel id="select-type" style={{color: '#001833', marginLeft: '10px'}}>Select Activity Type</InputLabel>
           <Select
-            lableID="select-type"
+            labelID="select-type"
             id="select-type"
-            lable="Select Activity Type"
+            label="Select Activity Type"
+            variant="outlined"
           >
             <MenuItem value={10}>Athletics/Recreation</MenuItem>
             <MenuItem value={20}>Museum/Education</MenuItem>
@@ -87,12 +151,15 @@ const Entertainment = () => {
 
   const SelectDistance = () => {
     return(
-      <FormControl fullWidth>
-        <InputLabel id="select-distance">Select Distance from E7</InputLabel>
+      <FormControl style={{backgroundColor: 'white', width: '300px', borderRadius: '15px', marginLeft: '20px'}}>
+        <InputLabel id="select-distance" style={{color: '#001833', marginLeft: '10px'}}>Select Distance from E7</InputLabel>
           <Select
             lableID="select-distance"
-            id="select-distacne"
-            lable="Select Distacne from E7"
+            id="select-distance"
+            label="Select Distance from E7"
+            variant='outlined'
+            align='center'
+            color= 'white'
           >
             <MenuItem value={10}>0 - 5km</MenuItem>
             <MenuItem value={20}>5km - 15km</MenuItem>
@@ -108,38 +175,52 @@ const Entertainment = () => {
     
     return(
       <Button 
-      variant="contained" 
-      color="primary"
+      variant="outlined" 
+      style={{backgroundColor: 'green', color: '#FFFFFF'}}
       >
-      Submit Your Filters
+      Search with Filters
       </Button>
     );
   };
 
+  const ClearButton = () => {
+    
+    return(
+      <Button 
+      variant="outlined" 
+      style={{backgroundColor: 'red', color: '#FFFFFF', marginLeft: "20px"}}
+      href='/Entertainment'
+      >
+      Clear All Filters
+      </Button>
+    );
+  };
 
   return (
-    <div>
-      <div>
+    <div className='backdropEnt'>
       <Grid item xs={12}>
-        <Paper className={classes.paper}>
-          <Typography align="center"
-            variany="h3">
-            Entertainment
-          </Typography>
-        </Paper>
-      </Grid>
-      </div>
-
-      <Grid item xs={12}>
-        <Paper className={classes.papper} align="center">
+      <Title></Title>
+        <div style={{marginTop: '50px'}}>
+        <Card align="center" style={{color:'#001833', backgroundColor: '#3f51b5'}}>
+        <div style={{marginTop: '20px'}}>
+        <PrefTitle></PrefTitle>
+</div>
+        <div style={{marginTop: '20px'}}>
           <SelectBudget/>
-          <SelectDistance/>
-          <SelectType/>
-          <SelectActivityLevel/>
-          <SubmitButton/>
-        </Paper>
-      </Grid>
 
+          <SelectDistance/>
+
+          <SelectType/>
+
+          <SelectActivityLevel/>
+</div>
+          <div style={{marginTop: '30px', marginBottom: '30px'}}>
+          <SubmitButton/>
+          <ClearButton/>
+          </div>
+        </Card>
+        </div>
+      </Grid>
     </div>
   );
 };
